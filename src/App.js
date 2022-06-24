1a3f59dd3d234e77fcd4125a411692a25e2ceab0
Eyüp Sercan UYGUR <sercanuygur@gmail.com>
2022-06-24 17:10:46 +0300

    Add empty routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import JoinRoomPage from "./JoinRoomPage/JoinRoomPage";
+import RoomPage from "./RoomPage/RoomPage";
+import IntroductionPage from "./IntroductionPage/IntroductionPage";
 
 function App() {
-  return <div className="App">Hello it's a React App from Sercan!</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/join-room" element={<JoinRoomPage />} />
+        <Route path="/room" element={<RoomPage />} />
+        <Route path="/" element={<IntroductionPage />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
