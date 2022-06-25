723cdb029a8fac07f2feb95f0742f876d4f8b0c9
Eyüp Sercan UYGUR <sercanuygur@gmail.com>
2022-06-25 19:56:50 +0300

    Add server call, set identiy on store

--- src/JoinRoomPage/JoinRoomContent.js
+++ src/JoinRoomPage/JoinRoomContent.js
@@ -2,19 +2,60 @@ import React, { useState } from "react";
 import JoinRoomInputs from "./JoinRoomInputs";
 import { connect } from "react-redux";
 import OnlyWithAudioCheckbox from "./OnlyWithAudioCheckbox";
-import { setConnectOnlyWithAudio } from "../store/actions";
+import {
+  setConnectOnlyWithAudio,
+  setIdentity,
+  setRoomId,
+} from "../store/actions";
 import ErrorMessage from "./ErrorMessage";
 import JoinRoomButtons from "./JoinRoomButtons";
+import { getRoomExist } from "../utils/api";
+import { useNavigate } from "react-router-dom";
 
 const JoinRoomContent = (props) => {
-  const { isRoomHost, setConnectOnlyWithAudio, connectOnlyWithAudio } = props;
+  const {
+    isRoomHost,
+    setConnectOnlyWithAudio,
+    connectOnlyWithAudio,
+    setIdentityAction,
+    setRoomIdAction,
+  } = props;
 
   const [roomIdValue, setRoomIdValue] = useState("");
   const [nameValue, setNameValue] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
 
-  const handleJoinRoom = () => {};
+  const navigate = useNavigate();
 
+  const handleJoinRoom = async () => {
+    setIdentityAction(nameValue);
+    if (isRoomHost) {
+      createRoom();
+    } else {
+      await joinRoom();
+    }
+  };
+
+  const joinRoom = async () => {
+    const responseMessage = await getRoomExist(roomIdValue);
+
+    const { roomExist, full } = responseMessage;
+
+    if (roomExist) {
+      if (full) {
+        setErrorMessage("Meeting reached the max amount of participitans!");
+      } else {
+        setRoomIdAction(roomIdValue);
+        navigate("/room");
+      }
+    } else {
+      setErrorMessage("Room not found. Check your meeting ID!");
+    }
+  };
+
+  const createRoom = () => {
+    navigate("/room");
+  };
   return (
     <>
       <JoinRoomInputs
@@ -47,6 +88,8 @@ const mapActionsToProps = (dispact) => {
   return {
     setConnectOnlyWithAudio: (onlyWithAudio) =>
       dispact(setConnectOnlyWithAudio(onlyWithAudio)),
+    setIdentityAction: (identity) => dispact(setIdentity(identity)),
+    setRoomIdAction: (roomId) => dispact(setRoomId(roomId)),
   };
 };
 
